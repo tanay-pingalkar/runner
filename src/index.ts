8449af912fb00a19181bdac1ae442e2a71fde6ee
tanay-pingalkar <tpingalkar@gmail.com>
2021-03-31 13:01:11 +0530

    added gist support

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 import chalk from "chalk";
 import { Runner } from "./processes/runner";
-import { runner_app } from "./processes/runner-app";
+import { runner_app } from "./processes/interactive";
 import { fileName, _2dArray } from "./typescript/types";
 import { readFile } from "./utils/readFile";
+import { fetchFile } from "./utils/fetchFile";
 
-export const cli = (args: Array<string>) => {
+export const cli = async (args: Array<string>) => {
   let fileName: fileName = "setup";
+  let file: string = "";
   if (args[2]) {
     fileName = args[2];
   }
-  const file: string = readFile(fileName);
+  if (fileName.startsWith("gist:")) {
+    fileName = fileName.split(":")[1];
+    file = await fetchFile(fileName);
+  } else {
+    file = readFile(fileName);
+  }
   new Runner(file);
 };
 
 export const runner = () => {
-  console.log(chalk.cyan("welcome to runner \ntype EXIT to exit"));
+  console.log(
+    chalk.cyan(
+      "welcome to runner \ntype EXIT to exit \n Type CHEATSHEET to know more"
+    )
+  );
   runner_app();
 };
---
+++ src/utils/fetchFile.ts
@@ -0,0 +1,28 @@
+import { Octokit } from "@octokit/core";
+import { exit } from "process";
+import { err } from "./err";
+import { OctokitResponse } from "@octokit/types/dist-types/OctokitResponse";
+import ora from "ora";
+
+export const fetchFile = async (gist_id: string): Promise<string> => {
+  const octokit = new Octokit();
+  let res: OctokitResponse<any, number>;
+  const spinner = ora("fetchining file").start();
+  try {
+    res = await octokit.request(`GET /gists/${gist_id}`);
+  } catch (error) {
+    err(error);
+    spinner.fail();
+    exit();
+  }
+  spinner.succeed();
+  const firstKey: string = Object.keys(res.data.files)[0];
+  let content: string;
+  if (firstKey.endsWith(".rn")) {
+    content = res.data.files[firstKey].content;
+  } else {
+    err(`${firstKey} is not a valid runner file`);
+    exit();
+  }
+  return content;
+};
